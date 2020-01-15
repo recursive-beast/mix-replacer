3322583d29dbf7ae3bd50a4dcf91c85668814cd9
soufiane yakoubi <splentercell.1997@gmail.com>
2020-01-16 00:57:18 +0100

    extract normalizeDir from CopyTask's constructor

--- src/CopyTask.js
+++ src/CopyTask.js
@@ -4,13 +4,24 @@ module.exports = class CopyTask {
 	constructor(src, target_dir = "") {
 		if (!fs.existsSync(src)) throw src + " doesn't exist";
 
-		if (Config.publicPath && !target_dir.startsWith(Config.publicPath))
-			target_dir = path.join(Config.publicPath, target_dir);
+		target_dir = this.normalizeDir(target_dir);
 
 		var fileName = path.basename(src);
 
 		this.src = src;
 
 		this.target = path.join(target_dir, fileName);
+	}
+
+	/**
+	 * prepare the provided directory path for processing
+	 * @param {string} dir
+	 */
+	normalizeDir(dir = "") {
+		var segments = dir.split(/\/|\\/g);
+
+		if (segments[0] === Config.publicPath) return dir;
+
+		return path.join(Config.publicPath, ...segments);
 	}
 };
