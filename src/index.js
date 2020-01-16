4f839517cf364ea07c70ecbe382897f0c2683eae
soufiane yakoubi <splentercell.1997@gmail.com>
2020-01-16 02:34:12 +0100

    create copy method in the Replacer class

--- src/index.js
+++ src/index.js
@@ -1,5 +1,29 @@
+const glob = require("glob");
+const CopyTask = require("./CopyTask");
+
 module.exports = class Replacer {
-	constructor() {}
+	constructor() {
+		/** @type {Map<string,CopyTask>} */
+		this.queue = new Map();
+	}
+
+	/**
+	 * copy all files extracted from `globPattern` to the target directory `target_dir`
+	 * @param {string} globPattern
+	 * @param {string} target_dir
+	 */
+	copy(globPattern, target_dir = "") {
+		const srcPaths = glob.sync(globPattern, { nodir: true });
+
+		for (var src of srcPaths) {
+			var task = this.queue.get(src);
+
+			if (task) task.setTargetDir(target_dir);
+			else this.queue.set(src, new CopyTask(src, target_dir));
+		}
+
+		return this;
+	}
 
 	apply(compiler) {
 		compiler.hooks.done.tap("Replacer", stats => {});
