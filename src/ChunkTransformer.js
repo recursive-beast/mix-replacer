6cc0589ac43a140958062a0c67e904c847f5e6ba
soufiane yakoubi <splentercell.1997@gmail.com>
2020-01-16 13:11:05 +0100

    create method refreshMAX in ChunkTransformer class

--- src/ChunkTransformer.js
+++ src/ChunkTransformer.js
@@ -1,3 +1,13 @@
 module.exports = class ChunkTransformer {
-	constructor() {}
+	constructor() {
+		this.MAX = 0;
+
+		this.refreshMAX();
+	}
+
+	refreshMAX() {
+		for (var key in Mix.manifest.manifest) {
+			if (key.length > this.MAX) this.MAX = key.length;
+		}
+	}
 };
