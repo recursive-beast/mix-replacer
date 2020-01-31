1b9991e61c07d2a61d9e5274e99f1f5728607165
soufiane yakoubi <splentercell.1997@gmail.com>
2020-01-31 23:30:02 +0100

    replace the Replacer class with a mix extension

--- src/index.js
+++ src/index.js
@@ -1,56 +1,43 @@
 const glob = require("glob");
+const mix = require("laravel-mix");
 const CopyTask = require("./CopyTask");
 
-module.exports = class Replacer {
-	constructor() {
-		/** @type {Object<string,CopyTask>} */
-		this._tasks = {};
+var tasks = {};
+
+mix.then(() => {
+	const runningTasks = [];
+
+	for (const src in tasks) {
+		let task = tasks[src];
+
+		runningTasks.push(task.run());
 	}
 
-	/**
-	 * copy all files extracted from `globPattern` to the target directory `target_dir`
-	 * @param {string} globPattern
-	 * @param {string} target_dir
-	 */
-	copy(globPattern, target_dir = "") {
-		const srcPaths = glob.sync(globPattern, { nodir: true });
-
-		for (var src of srcPaths) {
-			// if a task that has `src` as its src path is found
-			// override its target directory
-			// else add a new task
-
-			var task = this._tasks[src];
-
-			if (task) {
-				task.setTargetDir(target_dir);
-			} else {
-				this._tasks.set[src] = new CopyTask(src, target_dir);
-			}
+	Promise.allSettled(runningTasks).then(results => {
+		for (const { status, reason } of results) {
+			if (status === "rejected") console.log(reason);
 		}
 
-		return this;
+		Mix.manifest.refresh();
+	});
+
+	tasks = {};
+});
+
+mix.extend("copyAndReplace", (_, globPattern, target_dir = "") => {
+	const srcPaths = glob.sync(globPattern, { nodir: true });
+
+	for (var src of srcPaths) {
+		// if a task that has `src` as its src path is found
+		// override its target directory
+		// else add a new task
+
+		var task = tasks[src];
+
+		if (task) {
+			task.setTargetDir(target_dir);
+		} else {
+			tasks[src] = new CopyTask(src, target_dir);
+		}
 	}
-
-	apply(compiler) {
-		compiler.hooks.done.tap("Replacer", () => {
-			const runningTasks = [];
-
-			for (const src in this._tasks) {
-				let task = this._tasks[src];
-
-				runningTasks.push(task.run());
-			}
-
-			Promise.allSettled(runningTasks).then(results => {
-				for (const { status, reason } of results) {
-					if (status === "rejected") console.log(reason);
-				}
-
-				Mix.manifest.refresh();
-			});
-
-			this._tasks = {};
-		});
-	}
-};
+});
